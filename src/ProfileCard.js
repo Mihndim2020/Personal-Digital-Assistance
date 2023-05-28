901f65ae6184bc79302d313066a69a569fa9c0c0
Mihndim <mihndim2016@gmail.com>
2023-05-28 12:58:58 +0100

    Add props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function ProfileCard() {
-  return <div>Profile card !!!</div>;
+function ProfileCard({ title, handle, image }) {
+  return (
+    <div>
+      <img src={image} alt="display_image" />
+      <div>The Title is {title}</div>
+      <div>The handle is {handle}</div>
+    </div>
+  );
 }
 
 export default ProfileCard;
