34f567464d13814a5eaf4aa419b961227e95a192
Mihndim <mihndim2016@gmail.com>
2023-05-28 11:47:42 +0100

    Add props to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import ProfileCard from "./ProfileCard";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello PDA </h1>
+      <h1>Personal Digital Assistant </h1>
+
+      <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Sirri" handle="@sirri32" />
+      <ProfileCard />
+      <ProfileCard />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ProfileCard() {
-  return <div>Profile card</div>;
+  return <div>Profile card !!!</div>;
 }
 
 export default ProfileCard;
