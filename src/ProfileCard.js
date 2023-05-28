fa21d4b80e9536a64309f83129966bc900f04987
Mihndim <mihndim2016@gmail.com>
2023-05-28 14:25:13 +0100

    Add styling with bulma

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,10 +2,20 @@ import React from "react";
 
 function ProfileCard({ title, handle, image }) {
   return (
-    <div>
-      <img src={image} alt="display_image" />
-      <div>The Title is {title}</div>
-      <div>The handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="display_image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
